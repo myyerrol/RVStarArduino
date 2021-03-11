f5703ce74d43463e47233548ca9c1b436ed1211a
myyerrol <myyerrol@126.com>
2021-03-11 12:29:29 +0800

    Update to 21.03.11

--- src/RVStarArduino.c
+++ src/RVStarArduino.c
@@ -202,18 +202,18 @@ void initTimerPWMFunc(uint8_t pin) {
     uint32_t timer           = TIMER_ARR[pin];
     uint32_t timer_ch        = TIMER_CH_ARR[pin];
 
-    // 配置定时器时钟
-    // Configure timer clock
+    // 启用定时器时钟
+    // Enable timer clock
     rcu_periph_clock_enable(timer_gpio_clk);
     rcu_periph_clock_enable(RCU_AF);
     rcu_periph_clock_enable(timer_clk);
 
-    // 初始化定时器GPIO参数
-    // Initialize timer gpio parameter
+    // 初始化定时器GPIO参数（模式为推挽输出）
+    // Initialize timer gpio parameter(mode is push-pull)
     gpio_init(timer_gpio_port, GPIO_MODE_AF_PP, GPIO_OSPEED_50MHZ,
               timer_gpio_pin);
 
-    // 重置定时器
+    // 复位定时器
     // Reset timer
     timer_deinit(timer);
 
@@ -245,7 +245,7 @@ void initTimerPWMFunc(uint8_t pin) {
     timer_ocinitpara.ocidlestate  = TIMER_OC_IDLE_STATE_LOW;
     timer_ocinitpara.ocnidlestate = TIMER_OCN_IDLE_STATE_LOW;
 
-    // 启动定时器通道输出
+    // 启用定时器通道输出
     // Enable timer output
     timer_channel_output_config(timer, timer_ch, &timer_ocinitpara);
 
@@ -253,12 +253,13 @@ void initTimerPWMFunc(uint8_t pin) {
     // Configure timer channel in PWM0 mode
     timer_channel_output_pulse_value_config(timer, timer_ch, 255);
     timer_channel_output_mode_config(timer, timer_ch, TIMER_OC_MODE_PWM0);
-    timer_channel_output_shadow_config(timer, timer_ch, TIMER_OC_SHADOW_DISABLE);
+    timer_channel_output_shadow_config(timer, timer_ch,
+                                       TIMER_OC_SHADOW_DISABLE);
 
-    // 启动定时器自动重新加载功能
+    // 启用定时器自动重加载功能
     // Enable timer auto reload shadow function
     timer_auto_reload_shadow_enable(timer);
-    // 启动定时器
+    // 启用定时器
     // Enable timer
     timer_enable(timer);
 }
